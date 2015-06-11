3a6ec96c1c4d333b4caedaba1fd49cab0d3c1773
吴秋锋 <arthas.wuqf@gmail.com>
2015-06-11 18:59:47 +0800

    refactor validation filter

--- mapi/src/main/java/cn/momia/mapi/api/v1/AbstractApi.java
+++ mapi/src/main/java/cn/momia/mapi/api/v1/AbstractApi.java
@@ -3,7 +3,7 @@ package cn.momia.mapi.api.v1;
 import cn.momia.common.config.Configuration;
 import cn.momia.common.web.http.MomiaHttpClient;
 import cn.momia.common.web.http.MomiaHttpRequestExecutor;
-import cn.momia.common.web.http.MomiaHttpRequestSigner;
+import cn.momia.common.web.http.MomiaHttpRequestUtils;
 import cn.momia.common.web.secret.SecretKey;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +29,6 @@ public abstract class AbstractApi {
     protected void addSign(HttpServletRequest request, Map<String, String> params) {
         // TODO key 根据调用的服务不同，取不同的值
         String key = SecretKey.get("service");
-        params.put("sign", MomiaHttpRequestSigner.sign(params, key));
+        params.put("sign", MomiaHttpRequestUtils.sign(params, key));
     }
 }
