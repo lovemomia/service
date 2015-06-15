b653f12db066463adb11991eb350d180f81ef800
吴秋锋 <arthas.wuqf@gmail.com>
2015-06-15 11:33:53 +0800

    refactor comment service

--- service/src/main/java/cn/momia/service/base/comment/impl/CommentServiceImpl.java
+++ service/src/main/java/cn/momia/service/base/comment/impl/CommentServiceImpl.java
@@ -19,103 +19,65 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CommentServiceImpl extends DbAccessService implements CommentService {
-    public long addComment(final Comment comment, final long productId, final long skuId) {
-        final long customerId = comment.getCustomerId();
-        final long serverId = comment.getServerId();
-        final int star = comment.getStar();
-        final String content = comment.getContent();
+    @Override
+    public long add(final Comment comment) {
         KeyHolder keyHolder = new GeneratedKeyHolder();
         jdbcTemplate.update(new PreparedStatementCreator() {
             @Override
             public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-                String sql = "INSERT INTO t_comment(customerId,  serverId, skuId, productId, star, content, addTime) VALUES (?, ?, ?, ?, ?, ?, NOW())";
+                String sql = "INSERT INTO t_comment(customerId, serverId, productId, skuId, star, content, addTime) VALUES (?, ?, ?, ?, ?, ?, NOW())";
                 PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-                ps.setLong(1, customerId);
-                ps.setLong(2, serverId);
-                ps.setLong(3, skuId);
-                ps.setLong(4, productId);
-                ps.setInt(5, star);
-                ps.setString(6, content);
+                ps.setLong(1, comment.getCustomerId());
+                ps.setLong(2, comment.getServerId());
+                ps.setLong(3, comment.getProductId());
+                ps.setLong(4, comment.getSkuId());
+                ps.setInt(5, comment.getStar());
+                ps.setString(6, comment.getContent());
                 return ps;
             }
         }, keyHolder);
+
         return keyHolder.getKey().longValue();
-
-    }
-
-    @Override
-    public long add(Comment comment,long productId,long skuId) {
-        return addComment(comment,productId,skuId);
-    }
-
-    public Comment buildComment(ResultSet resultSet) throws SQLException {
-        Comment comment = new Comment();
-        comment.setId(resultSet.getLong("id"));
-        comment.setCustomerId(resultSet.getLong("customerId"));
-        comment.setServerId(resultSet.getLong("serverId"));
-        comment.setSkuId(resultSet.getLong("skuId"));
-        comment.setProductId(resultSet.getLong("productId"));
-        comment.setStar(resultSet.getInt("star"));
-        comment.setContent(resultSet.getString("content"));
-        return comment;
     }
 
     @Override
     public Comment get(long id) {
-        String sql = "select id, customerId, serverId, skuId, productId, star, content from t_comment where id=? and status=1";
+        String sql = "select id, customerId, serverId, productId, skuId, star, content from t_comment where id=? and status=1";
+
         return jdbcTemplate.query(sql, new Object[] { id }, new ResultSetExtractor<Comment>() {
             @Override
-            public Comment extractData(ResultSet resultSet) throws SQLException, DataAccessException {
-                if (resultSet.next()) return buildComment(resultSet);
+            public Comment extractData(ResultSet rs) throws SQLException, DataAccessException {
+                if (rs.next()) return buildComment(rs);
                 return Comment.NOT_EXIST_COMMENT;
             }
         });
     }
 
-    public List<Comment> queryByPage(int TYPE, long id, int star, int start, int count) {
-        String name = "";
-        String sql = "";
-        Object[] objects;
-        switch (TYPE){
-            case 0: name = "skuId";
-            case 1: name = "serverId";
-            case 2: name = "productId";
-        }
-        while (start >= 0 && count > 0) {
-            if (star == 0) {
-                sql = "select id, customerId, serverId, skuId, productId, star,content from t_comment where " + name + "=? and status=1 limit ?,?";
-                objects = new Object[] { id, start, count };
-            } else {
-                sql = "select id, customerId, serverId, skuId, productId, star,content from t_comment where " + name + "=? and star=? and status=1 limit ?,?";
-                objects = new Object[] { id, star, start, count };
+    public Comment buildComment(ResultSet rs) throws SQLException {
+        Comment comment = new Comment();
+        comment.setId(rs.getLong("id"));
+        comment.setCustomerId(rs.getLong("customerId"));
+        comment.setServerId(rs.getLong("serverId"));
+        comment.setProductId(rs.getLong("productId"));
+        comment.setSkuId(rs.getLong("skuId"));
+        comment.setStar(rs.getInt("star"));
+        comment.setContent(rs.getString("content"));
+
+        return comment;
+    }
+
+    @Override
+    public List<Comment> queryByProduct(long productId, int start, int count) {
+        final List<Comment> comments = new ArrayList<Comment>();
+
+        String sql = "select id, customerId, serverId, productId, skuId, star,content from t_comment where productId=? and status=1 limit ?,?";
+        jdbcTemplate.query(sql, new Object[] { productId, start, count }, new RowCallbackHandler() {
+            @Override
+            public void processRow(ResultSet rs) throws SQLException {
+                comments.add(buildComment(rs));
             }
-            final List<Comment> comments = new ArrayList<Comment>();
-            jdbcTemplate.query(sql, objects, new RowCallbackHandler() {
-                @Override
-                public void processRow(ResultSet resultSet) throws SQLException {
-                    comments.add(buildComment(resultSet));
-                }
-            });
-            return comments;
-        }
-        return null;
-    }
+        });
 
-    @Override
-    public List<Comment> queryBySku(long skuId, int star, int start, int count) {
-        int TYPE = Comment.Type.SKU;
-        return queryByPage(TYPE, skuId, star, start, count);
-    }
-
-    @Override
-    public List<Comment> queryByServer(long serverId, int star, int start, int count) {
-        int TYPE = Comment.Type.SERVER;
-        return queryByPage(TYPE, serverId, star, start, count);
-    }
-
-    @Override
-    public List<Comment> queryByProduct(long productId, int star, int start, int count) {
-        int TYPE = Comment.Type.PRODUCT;
-        return queryByPage(TYPE, productId, star, start, count);
+        return comments;
     }
 }
