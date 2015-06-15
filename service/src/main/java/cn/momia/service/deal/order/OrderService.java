b5b5e105099c76c02bc432cde87b34a016e78fba
吴秋锋 <arthas.wuqf@gmail.com>
2015-06-15 15:16:08 +0800

    refactor order service

--- service/src/main/java/cn/momia/service/deal/order/OrderService.java
+++ service/src/main/java/cn/momia/service/deal/order/OrderService.java
@@ -8,6 +8,5 @@ public interface OrderService {
     long add(Order order);
     Order get(long id);
     List<Order> queryByProduct(long productId, int start, int count);
-    List<User> queryUserByProduct(long productId, int start, int count);
     boolean pay(long id);
 }
