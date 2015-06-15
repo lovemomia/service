50e98fe9e911a74857e0d344a42f04b78f0334e3
吴秋锋 <arthas.wuqf@gmail.com>
2015-06-15 17:13:32 +0800

    refactor sms

--- service/src/main/java/cn/momia/service/sms/SmsSender.java
+++ service/src/main/java/cn/momia/service/sms/SmsSender.java
@@ -1,7 +1,5 @@
 package cn.momia.service.sms;
 
-import java.text.ParseException;
-
 public interface SmsSender {
-    boolean send(String phone) throws ParseException;
+    boolean send(String mobile);
 }
