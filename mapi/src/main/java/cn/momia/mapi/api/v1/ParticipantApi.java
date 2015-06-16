5dd25889b1994c08921b25a986fa6c230234e4e5
吴秋锋 <arthas.wuqf@gmail.com>
2015-06-16 14:59:04 +0800

    add version v1

--- mapi/src/main/java/cn/momia/mapi/api/v1/ParticipantApi.java
+++ mapi/src/main/java/cn/momia/mapi/api/v1/ParticipantApi.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/participant")
+@RequestMapping("/v1/participant")
 public class ParticipantApi extends AbstractApi {
     @RequestMapping(method = RequestMethod.POST)
     public ResponseMessage addParticipant(@RequestParam String utoken, @RequestParam String participant) {
