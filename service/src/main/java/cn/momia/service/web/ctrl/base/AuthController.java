d3e8053492dc5e5e9419356c09a003fb2d29e7a9
吴秋锋 <arthas.wuqf@gmail.com>
2015-06-16 10:50:47 +0800

    rm not used import

--- service/src/main/java/cn/momia/service/web/ctrl/base/AuthController.java
+++ service/src/main/java/cn/momia/service/web/ctrl/base/AuthController.java
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.text.ParseException;
-
 @RestController
 @RequestMapping("/auth")
 public class AuthController extends AbstractController {
