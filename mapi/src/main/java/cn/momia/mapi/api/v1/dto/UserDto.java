80e87d7f7ee29e0e01660541e94048a778c4bdfc
吴秋锋 <arthas.wuqf@gmail.com>
2015-06-23 14:27:35 +0800

    rm set method

--- mapi/src/main/java/cn/momia/mapi/api/v1/dto/UserDto.java
+++ mapi/src/main/java/cn/momia/mapi/api/v1/dto/UserDto.java
@@ -42,64 +42,32 @@ public class UserDto implements Dto {
         return token;
     }
 
-    public void setToken(String token) {
-        this.token = token;
-    }
-
     public String getMobile() {
         return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
     }
 
     public String getAvatar() {
         return avatar;
     }
 
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getSex() {
         return sex;
     }
 
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
     public Date getBirthday() {
         return birthday;
-    }
-
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
     }
 
     public int getCityId() {
         return cityId;
     }
 
-    public void setCityId(int cityId) {
-        this.cityId = cityId;
-    }
-
     public String getAddress() {
         return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 
     protected UserDto(JSONObject userJson) {
