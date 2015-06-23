8e8e55d7108f068d25ed5dc32fa72623ccffebc2
吴秋锋 <arthas.wuqf@gmail.com>
2015-06-23 11:00:39 +0800

    add logging in auth controller

--- service/src/main/java/cn/momia/service/web/ctrl/base/AuthController.java
+++ service/src/main/java/cn/momia/service/web/ctrl/base/AuthController.java
@@ -10,15 +10,21 @@ import cn.momia.service.sms.SmsVerifier;
 import cn.momia.service.web.ctrl.AbstractController;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController extends AbstractController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
+
     @Autowired
     private SmsSender smsSender;
 
@@ -30,10 +36,13 @@ public class AuthController extends AbstractController {
 
     @RequestMapping(value = "/send", method = RequestMethod.POST)
     public ResponseMessage send(@RequestParam String mobile) {
-        boolean successful = smsSender.send(mobile);
-
-        if (!successful) return new ResponseMessage(ErrorCode.INTERNAL_SERVER_ERROR, "fail to send verify code");
-        return new ResponseMessage("send verify code successfully");
+        try {
+            smsSender.send(mobile);
+            return ResponseMessage.SUCCESS;
+        } catch (Exception e) {
+            LOGGER.error("fail to send verify code for {}", mobile, e);
+            return new ResponseMessage(ErrorCode.INTERNAL_SERVER_ERROR, "fail to send verify code");
+        }
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
@@ -41,15 +50,28 @@ public class AuthController extends AbstractController {
         if (StringUtils.isBlank(mobile) || StringUtils.isBlank(code))
             return new ResponseMessage(ErrorCode.BAD_REQUEST, "mobile or(and) verify code is empty");
 
-        if (!smsVerifier.verify(mobile, code)) return new ResponseMessage(ErrorCode.FORBIDDEN, "fail to verify");
+        if (!smsVerifier.verify(mobile, code)) return new ResponseMessage(ErrorCode.FORBIDDEN, "fail to verify code");
 
         User user = userService.getByMobile(mobile);
-        if (!user.exists()) user = userService.add(mobile, generateToken(mobile));
+        String token = generateToken(mobile);
+        if (!user.exists()) {
+            user = userService.add(mobile, token);
+            if (!user.exists()) {
+                LOGGER.error("fail to add user for {}", mobile);
+                return new ResponseMessage(ErrorCode.INTERNAL_SERVER_ERROR, "fail to login");
+            }
+        } else {
+            if (!userService.updateToken(user.getId(), token)) {
+                LOGGER.warn("fail to update token for {}, will use old token", mobile);
+            } else {
+                user.setToken(token);
+            }
+        }
 
         return new ResponseMessage(user);
     }
 
     private String generateToken(String mobile) {
-        return DigestUtils.md5Hex(StringUtils.join(new String[] { mobile, SecretKey.get() }, "|"));
+        return DigestUtils.md5Hex(StringUtils.join(new String[] { mobile, new Date().toString(), SecretKey.get() }, "|"));
     }
 }
