f0a97635b396ea327e73979ad696e2b952863e67
吴秋锋 <arthas.wuqf@gmail.com>
2015-06-24 13:37:58 +0800

    add fields in skuPrice

--- service/src/main/java/cn/momia/service/base/product/sku/SkuPrice.java
+++ service/src/main/java/cn/momia/service/base/product/sku/SkuPrice.java
@@ -1,21 +1,37 @@
 package cn.momia.service.base.product.sku;
 
+import com.alibaba.fastjson.JSONObject;
+
 import java.io.Serializable;
 
 public class SkuPrice implements Serializable {
-    private String name;
-    private float price;
+    private static final String[] UNIT = new String[] { "人", "组" };
 
-    public String getName() {
-        return name;
+    private int adult;
+    private int child;
+    private float price;
+    private String unit;
+
+    public int getAdult() {
+        return adult;
+    }
+
+    public int getChild() {
+        return child;
     }
 
     public float getPrice() {
         return price;
     }
 
-    public SkuPrice(String name, float price) {
-        this.name = name;
-        this.price = price;
+    public String getUnit() {
+        return unit;
+    }
+
+    public SkuPrice(JSONObject priceJson) {
+        this.adult = priceJson.getInteger("adult");
+        this.child = priceJson.getInteger("child");
+        this.price = priceJson.getFloat("price");
+        this.unit = UNIT[priceJson.getInteger("unit")];
     }
 }
