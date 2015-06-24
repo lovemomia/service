1f3b9e218d5c70555fab2089aed90b9c949bae35
吴秋锋 <arthas.wuqf@gmail.com>
2015-06-24 10:41:01 +0800

    rm main

--- common/web/src/main/java/cn/momia/common/web/http/impl/MomiaHttpPostRequest.java
+++ common/web/src/main/java/cn/momia/common/web/http/impl/MomiaHttpPostRequest.java
@@ -1,21 +1,10 @@
 package cn.momia.common.web.http.impl;
 
-import com.alibaba.fastjson.JSON;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpEntityEnclosingRequest;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicHeader;
 import org.apache.http.protocol.HTTP;
-import org.apache.http.util.EntityUtils;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
@@ -49,31 +38,5 @@ public class MomiaHttpPostRequest extends AbstractMomiaEntityEnclosingHttpReques
 
         setEntity(entity);
         httpRequestBase = httpPost;
-    }
-
-    public static void main(String[] args) {
-        HttpPost httpPost = new HttpPost("http://localhost:8080/participant");
-        httpPost.addHeader(HTTP.CONTENT_TYPE, "application/json");
-
-        try {
-            StringEntity entity = new StringEntity("{\"birthday\": \"2016-03-18\",\"name\": \"小吴\",\"sex\": 1}", "utf-8");
-            entity.setContentType("application/json");
-            entity.setContentEncoding("utf-8");
-            httpPost.setEntity(entity);
-
-            HttpClient httpClient = HttpClients.createDefault();
-            HttpResponse response = httpClient.execute(httpPost);
-            if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
-                throw new RuntimeException("fail to execute request: " + httpClient);
-            }
-
-            System.out.println(EntityUtils.toString(response.getEntity()));
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
