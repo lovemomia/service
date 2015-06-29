cbf0421df4d24541491cb0fe1ff8db536efbfda8
吴秋锋 <arthas.wuqf@gmail.com>
2015-06-29 13:53:01 +0800

    verify mobile

--- mapi/src/main/java/cn/momia/mapi/api/v1/AuthApi.java
+++ mapi/src/main/java/cn/momia/mapi/api/v1/AuthApi.java
@@ -12,19 +12,31 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.regex.Pattern;
+
 @RestController
 @RequestMapping("/v1/auth")
 public class AuthApi extends AbstractApi {
+    private static final Pattern MOBILE_PATTERN = Pattern.compile("^1[0-9]{10}$");
+
     @RequestMapping(value = "/send", method = RequestMethod.POST)
     public ResponseMessage send(@RequestParam String mobile)  {
+        if (isInvalidMobile(mobile)) return ResponseMessage.FAILED("invalid mobile");
+
         MomiaHttpParamBuilder builder = new MomiaHttpParamBuilder().add("mobile", mobile);
         MomiaHttpRequest request = MomiaHttpRequest.POST(baseServiceUrl("auth/send"), builder.build());
 
         return executeRequest(request);
     }
 
+    private boolean isInvalidMobile(String mobile) {
+        return !MOBILE_PATTERN.matcher(mobile).find();
+    }
+
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public ResponseMessage login(@RequestParam String mobile, @RequestParam String code) {
+        if (isInvalidMobile(mobile)) return ResponseMessage.FAILED("invalid mobile");
+
         MomiaHttpParamBuilder builder = new MomiaHttpParamBuilder()
                 .add("mobile", mobile)
                 .add("code", code);
@@ -40,6 +52,8 @@ public class AuthApi extends AbstractApi {
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public ResponseMessage register(@RequestParam String nickName, @RequestParam String mobile, @RequestParam String code) {
+        if (isInvalidMobile(mobile)) return ResponseMessage.FAILED("invalid mobile");
+
         MomiaHttpParamBuilder builder = new MomiaHttpParamBuilder()
                 .add("nickName", nickName)
                 .add("mobile", mobile)
