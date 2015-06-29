f8114632622dca5bb03894fe442a57acb89c68da
ysm9099 <909912314@qq.com>
2015-06-29 17:09:09 +0800

    modify ParticipantDto

--- mapi/src/main/java/cn/momia/mapi/api/v1/dto/base/ParticipantDto.java
+++ mapi/src/main/java/cn/momia/mapi/api/v1/dto/base/ParticipantDto.java
@@ -3,6 +3,7 @@ package cn.momia.mapi.api.v1.dto.base;
 import com.alibaba.fastjson.JSONObject;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class ParticipantDto implements Dto {
@@ -48,40 +49,21 @@ public class ParticipantDto implements Dto {
         this.name = participantJson.getString("name");
         this.sex = participantJson.getString("sex");
         this.birthday = participantJson.getDate("birthday");
-        Date currentTime = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        String nowTime = formatter.format(currentTime);
-        String myBirthday = formatter.format(this.birthday);
 
-        long day=getDays(nowTime,myBirthday);
-        long year=day/365;
-        if(year>=15)
+        Calendar calendar = Calendar.getInstance();
+        int yearNow = calendar.get(Calendar.YEAR);
+        calendar.setTime(this.birthday);
+        int yearBorn = calendar.get(Calendar.YEAR);
+
+        if (yearNow - yearBorn > 15)
             this.type = "成人";
         else
-           this.type = "儿童";
+            this.type = "儿童";
 
 
         if (showIdNo) {
             this.idType = participantJson.getInteger("idType");
             this.idNo = participantJson.getString("idNo");
         }
-    }
-
-    public static long getDays(String date1, String date2) {
-        if (date1 == null || date1.equals(""))
-            return 0;
-        if (date2 == null || date2.equals(""))
-            return 0;
-        // 转换为标准时间
-        SimpleDateFormat myFormatter = new SimpleDateFormat("yyyy-MM-dd");
-        java.util.Date date = null;
-        java.util.Date mydate = null;
-        try {
-            date = myFormatter.parse(date1);
-            mydate = myFormatter.parse(date2);
-        } catch (Exception e) {
-        }
-        long day = (date.getTime() - mydate.getTime()) / (24 * 60 * 60 * 1000);
-        return Math.abs(day);
     }
 }
