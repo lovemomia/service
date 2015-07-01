d9ff04ba0054d9639349923a178ba19f294ea507
ysm9099 <909912314@qq.com>
2015-07-01 11:41:20 +0800

    modify

--- mapi/src/main/java/cn/momia/mapi/api/v1/ProductApi.java
+++ mapi/src/main/java/cn/momia/mapi/api/v1/ProductApi.java
@@ -168,7 +168,7 @@ public class ProductApi extends AbstractApi {
 
     @RequestMapping(value = "/order", method = RequestMethod.GET)
     public ResponseMessage getProductOrder(@RequestParam long id, @RequestParam String utoken) {
-        if(utoken == null || utoken == "")
+        if(utoken == null || utoken.equals(""))
             return ResponseMessage.FAILED("please login in first");
         
         List<MomiaHttpRequest> requests = buildProductOrderRequests(id, utoken);
