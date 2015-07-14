ad1afaee8288f61b34fbc40feecc81b5053b7fa0
吴秋锋 <arthas.wuqf@gmail.com>
2015-07-14 09:49:34 +0800

    return type

--- mapi/src/main/java/cn/momia/mapi/api/v1/dto/base/CouponDto.java
+++ mapi/src/main/java/cn/momia/mapi/api/v1/dto/base/CouponDto.java
@@ -1,20 +1,26 @@
 package cn.momia.mapi.api.v1.dto.base;
 
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.annotation.JSONField;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
 public class CouponDto implements Dto {
     private int id;
+    private int type;
     private String title;
     private String desc;
     private BigDecimal discount;
-    private Date startTime;
-    private Date endTime;
+    @JSONField(format = "yyyy-MM-dd hh:mm:ss") private Date startTime;
+    @JSONField(format = "yyyy-MM-dd hh:mm:ss") private Date endTime;
 
     public int getId() {
         return id;
+    }
+
+    public int getType() {
+        return type;
     }
 
     public String getTitle() {
@@ -39,6 +45,7 @@ public class CouponDto implements Dto {
 
     public CouponDto(JSONObject couponJson) {
         this.id = couponJson.getInteger("id");
+        this.type = couponJson.getInteger("type");
         this.title = couponJson.getString("title");
         this.desc = couponJson.getString("desc");
         this.discount = couponJson.getBigDecimal("discount");
