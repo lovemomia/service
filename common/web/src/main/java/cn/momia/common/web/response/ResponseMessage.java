596eca709d507315e5b13c92effaa80c4c65e251
吴秋锋 <arthas.wuqf@gmail.com>
2015-07-16 18:06:35 +0800

    refactor http executor

--- common/web/src/main/java/cn/momia/common/web/response/ResponseMessage.java
+++ common/web/src/main/java/cn/momia/common/web/response/ResponseMessage.java
@@ -11,7 +11,7 @@ public class ResponseMessage implements Serializable
     public static final ResponseMessage SUCCESS = new ResponseMessage("success");
     public static final ResponseMessage FAILED = new ResponseMessage(ErrorCode.FAILED, "failed");
     public static final ResponseMessage BAD_REQUEST = new ResponseMessage(ErrorCode.BAD_REQUEST, "invalid params");
-    public static final ResponseMessage TOKEN_EXPIRED = new ResponseMessage(ErrorCode.TOKEN_EXPIRED, "user token expired");
+    public static final ResponseMessage TOKEN_EXPIRED = new ResponseMessage(ErrorCode.TOKEN_EXPIRED, "用户token过期，需要重新登录");
     public static final ResponseMessage EMPTY_MAP = new ResponseMessage(new JSONObject());
     public static final ResponseMessage EMPTY_ARRAY = new ResponseMessage(new JSONArray());
 
--- mapi/src/main/java/cn/momia/mapi/api/AbstractApi.java
+++ mapi/src/main/java/cn/momia/mapi/api/AbstractApi.java
@@ -2,14 +2,12 @@ package cn.momia.mapi.api;
 
 import cn.momia.common.config.Configuration;
 import cn.momia.common.web.controller.BaseController;
-import cn.momia.common.web.http.MomiaHttpClient;
 import cn.momia.common.web.http.MomiaHttpRequest;
 import cn.momia.common.web.http.MomiaHttpRequestExecutor;
 import cn.momia.common.web.http.MomiaHttpResponseCollector;
 import cn.momia.common.web.response.ErrorCode;
 import cn.momia.common.web.response.ResponseMessage;
 import cn.momia.mapi.api.v1.dto.base.Dto;
-import com.alibaba.fastjson.JSONObject;
 import com.google.common.base.Function;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,14 +18,8 @@ import java.util.List;
 public abstract class AbstractApi extends BaseController {
     private static final Logger LOGGER = LoggerFactory.getLogger(AbstractApi.class);
 
-    @Autowired
-    protected Configuration conf;
-
-    @Autowired
-    protected MomiaHttpClient httpClient;
-
-    @Autowired
-    protected MomiaHttpRequestExecutor requestExecutor;
+    @Autowired protected Configuration conf;
+    @Autowired protected MomiaHttpRequestExecutor requestExecutor;
 
     protected String url(Object... paths) {
         // TODO 根据paths判断使用哪个service
@@ -35,11 +27,8 @@ public abstract class AbstractApi extends BaseController {
     }
 
     private String serviceUrl(String service, Object... paths) {
-        StringBuilder urlBuilder = new StringBuilder();
-        urlBuilder.append(service);
-        for (Object path : paths) {
-            urlBuilder.append("/").append(path);
-        }
+        StringBuilder urlBuilder = new StringBuilder().append(service);
+        for (Object path : paths) urlBuilder.append("/").append(path);
 
         return urlBuilder.toString();
     }
@@ -49,25 +38,16 @@ public abstract class AbstractApi extends BaseController {
     }
 
     protected ResponseMessage executeRequest(MomiaHttpRequest request, Function<Object, Dto> buildResponseData) {
-        try {
-            JSONObject responseJson = httpClient.execute(request);
-            if (buildResponseData == null || responseJson.getInteger("errno") != ErrorCode.SUCCESS) return ResponseMessage.formJson(responseJson);
-            return new ResponseMessage(buildResponseData.apply(responseJson.get("data")));
-        } catch (Exception e) {
-            LOGGER.error("fail to execute request: {}", request, e);
-            return new ResponseMessage(ErrorCode.FAILED, "fail to execute request");
-        }
+        ResponseMessage responseMessage = requestExecutor.execute(request);
+
+        if (buildResponseData == null || !responseMessage.successful()) return responseMessage;
+        return new ResponseMessage(buildResponseData.apply(responseMessage.getData()));
     }
 
     protected ResponseMessage executeRequests(List<MomiaHttpRequest> requests, Function<MomiaHttpResponseCollector, Dto> buildResponseData) {
         MomiaHttpResponseCollector collector = requestExecutor.execute(requests);
-        if (collector.getErrnos().contains(ErrorCode.TOKEN_EXPIRED)) return ResponseMessage.TOKEN_EXPIRED;
 
-        if (!collector.isSuccessful()) {
-            LOGGER.error("fail to execute requests: {}, exceptions: {}", requests, collector.getExceptions());
-            return new ResponseMessage(ErrorCode.FAILED, "fail to execute requests");
-        }
-
+        if (collector.notLogin()) return ResponseMessage.TOKEN_EXPIRED;
         return new ResponseMessage(buildResponseData.apply(collector));
     }
 }
