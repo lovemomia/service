89b30d5699b42e3269ab2551742e929f615b4451
吴秋锋 <arthas.wuqf@gmail.com>
2015-07-22 12:04:52 +0800

    log

--- common/web/src/main/java/cn/momia/common/web/controller/BaseController.java
+++ common/web/src/main/java/cn/momia/common/web/controller/BaseController.java
@@ -4,11 +4,17 @@ import cn.momia.common.web.exception.MomiaExpiredException;
 import cn.momia.common.web.exception.MomiaFailedException;
 import cn.momia.common.web.response.ErrorCode;
 import cn.momia.common.web.response.ResponseMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 public abstract class BaseController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseController.class);
+
     @ExceptionHandler
     public ResponseMessage exception(Exception exception) {
+        LOGGER.error("exception!!", exception);
+
         if(exception instanceof MomiaFailedException) {
             return ResponseMessage.FAILED(exception.getMessage());
         } else if (exception instanceof MomiaExpiredException) {
@@ -17,5 +23,4 @@ public abstract class BaseController {
             return new ResponseMessage(ErrorCode.INTERNAL_SERVER_ERROR, "500 internal server error");
         }
     }
-
 }
