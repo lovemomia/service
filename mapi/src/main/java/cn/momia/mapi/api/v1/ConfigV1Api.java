68a80e98df2754263be51c6e66ba949a7f16ed17
吴秋锋 <arthas.wuqf@gmail.com>
2015-07-31 13:12:06 +0800

    method GET

--- mapi/src/main/java/cn/momia/mapi/api/v1/ConfigV1Api.java
+++ mapi/src/main/java/cn/momia/mapi/api/v1/ConfigV1Api.java
@@ -3,6 +3,7 @@ package cn.momia.mapi.api.v1;
 import cn.momia.common.service.config.Configuration;
 import cn.momia.common.web.response.ResponseMessage;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -11,6 +12,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/v1/config")
 public class ConfigV1Api {
+    @RequestMapping(method = RequestMethod.GET)
     public ResponseMessage getConfig() {
         Map<String, Object> config = new HashMap<String, Object>();
         config.put("showLeader", Configuration.getBoolean("App.ShowLeader"));
