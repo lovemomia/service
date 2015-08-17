24fd6b23b88cd84cc1af0f7f652f3a49fd608db2
吴秋锋 <arthas.wuqf@gmail.com>
2015-08-17 17:18:27 +0800

    自己注册为出行人

--- user/service-user/src/main/java/cn/momia/service/user/web/ctrl/AuthController.java
+++ user/service-user/src/main/java/cn/momia/service/user/web/ctrl/AuthController.java
@@ -3,15 +3,22 @@ package cn.momia.service.user.web.ctrl;
 import cn.momia.api.common.CommonServiceApi;
 import cn.momia.service.base.web.response.ResponseMessage;
 import cn.momia.service.user.base.User;
+import cn.momia.service.user.participant.Participant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController extends UserRelatedController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
+
     @Autowired private CommonServiceApi commonServiceApi;
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
@@ -26,6 +33,16 @@ public class AuthController extends UserRelatedController {
 
         User user = userServiceFacade.register(nickName, mobile, password);
         if (!user.exists()) return ResponseMessage.FAILED("注册失败");
+
+        try {
+            Participant participant = new Participant();
+            participant.setUserId(user.getId());
+            participant.setSex("女");
+            participant.setBirthday(new Date(0));
+            userServiceFacade.addParticipant(participant);
+        } catch (Exception e) {
+            LOGGER.error("fail to add participant for user: {}", user.getId());
+        }
 
         return ResponseMessage.SUCCESS(buildUserResponse(user));
     }
