5df40dd1e859a2f951720afb67cd240ba66d9602
吴秋锋 <arthas.wuqf@gmail.com>
2015-09-07 13:52:56 +0800

    mv base dtos

--- base/service-base/src/main/java/cn/momia/service/base/web/ctrl/dto/ListDto.java
+++
@@ -1,7 +0,0 @@
-package cn.momia.service.base.web.ctrl.dto;
-
-import java.util.ArrayList;
-
-public class ListDto extends ArrayList<Object> implements Dto {
-    public static final ListDto EMPTY = new ListDto();
-}
