42b60ddb30081f491489feacbef4d57eacf65ff9
吴秋锋 <arthas.wuqf@gmail.com>
2015-09-09 11:41:52 +0800

    去掉jersey

--- base/service-base/src/main/java/cn/momia/service/base/sms/impl/LuosimaoSmsSender.java
+++ base/service-base/src/main/java/cn/momia/service/base/sms/impl/LuosimaoSmsSender.java
@@ -4,15 +4,23 @@ import cn.momia.common.webapp.config.Configuration;
 import cn.momia.service.base.sms.SmsSender;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
-import com.sun.jersey.core.util.MultivaluedMapImpl;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LuosimaoSmsSender implements SmsSender {
     private static final Logger LOGGER = LoggerFactory.getLogger(LuosimaoSmsSender.class);
@@ -20,24 +28,30 @@ public class LuosimaoSmsSender implements SmsSender {
     @Override
     public boolean send(String mobile, String msg) {
         try {
-            // TODO 使用HttpClient
-            Client client = Client.create();
-            client.addFilter(new HTTPBasicAuthFilter("api", Configuration.getString("Sms.Luosimao.Key")));
-            WebResource webResource = client.resource(Configuration.getString("Sms.Luosimao.Service"));
-            MultivaluedMapImpl formData = new MultivaluedMapImpl();
-            formData.add("mobile", mobile);
-            formData.add("message", msg);
-            ClientResponse response =  webResource.type(MediaType.APPLICATION_FORM_URLENCODED).post(ClientResponse.class, formData);
-            int status = response.getStatus();
-            if (status != 200) {
+            HttpPost httpPost = new HttpPost(Configuration.getString("Sms.Luosimao.Service"));
+
+            String auth = "api:" + Configuration.getString("Sms.Luosimao.Key");
+            byte[] encodedAuth = Base64.encodeBase64(auth.getBytes());
+            String authHeader = "Basic " + new String(encodedAuth);
+            httpPost.setHeader(HttpHeaders.AUTHORIZATION, authHeader);
+
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("mobile", mobile));
+            params.add(new BasicNameValuePair("message", msg));
+            HttpEntity entity = new UrlEncodedFormEntity(params, "UTF-8");
+            httpPost.setEntity(entity);
+
+            HttpClient httpClient = HttpClientBuilder.create().build();
+            HttpResponse response = httpClient.execute(httpPost);
+            if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
                 LOGGER.error("fail to send msg to user, {}/{}", mobile, msg);
                 return false;
             }
 
-            String textEntity = response.getEntity(String.class);
-            JSONObject responseJson = JSON.parseObject(textEntity);
-            int error = responseJson.getInteger("error");
+            String responseEntity = EntityUtils.toString(response.getEntity());
+            JSONObject responseJson = JSON.parseObject(responseEntity);
 
+            int error = responseJson.getInteger("error");
             if (error == 0) return true;
 
             LOGGER.error("fail to send msg to user, {}/{}, error code is: {}", mobile, msg, error);
