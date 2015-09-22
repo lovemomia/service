7054e39d0549d95d2b155f70509d20da0ae6c639
吴秋锋 <arthas.wuqf@gmail.com>
2015-09-22 19:19:40 +0800

    使用CityDistrictsDto

--- base/service-base/src/main/java/cn/momia/service/base/web/ctrl/RegionController.java
+++ base/service-base/src/main/java/cn/momia/service/base/web/ctrl/RegionController.java
@@ -1,13 +1,14 @@
 package cn.momia.service.base.web.ctrl;
 
+import cn.momia.api.base.dto.CityDistrictsDto;
+import cn.momia.api.base.dto.CityDto;
+import cn.momia.api.base.dto.RegionDto;
 import cn.momia.common.api.http.MomiaHttpResponse;
 import cn.momia.common.webapp.ctrl.BaseController;
 import cn.momia.service.base.city.City;
 import cn.momia.service.base.city.CityService;
 import cn.momia.service.base.region.Region;
 import cn.momia.service.base.region.RegionService;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -46,15 +47,48 @@ public class RegionController extends BaseController {
             districts.add(region);
         }
 
-        JSONArray treeJson = new JSONArray();
+        List<CityDistrictsDto> cityDistrictsDtos = new ArrayList<CityDistrictsDto>();
         for (City city : cities) {
-            JSONObject cityDistrictsJson = new JSONObject();
-            cityDistrictsJson.put("city", city);
-            cityDistrictsJson.put("districts", districtsOfCities.get(city.getId()));
-
-            treeJson.add(cityDistrictsJson);
+            cityDistrictsDtos.add(buildCityDistrictsDto(city, districtsOfCities.get(city.getId())));
         }
 
-        return MomiaHttpResponse.SUCCESS(treeJson);
+        return MomiaHttpResponse.SUCCESS(cityDistrictsDtos);
+    }
+
+    private CityDistrictsDto buildCityDistrictsDto(City city, List<Region> districts) {
+        CityDistrictsDto cityDistrictsDto = new CityDistrictsDto();
+        cityDistrictsDto.setCity(buildCityDto(city));
+        cityDistrictsDto.setDistricts(buildRegionDtos(districts));
+
+        return cityDistrictsDto;
+    }
+
+    private CityDto buildCityDto(City city) {
+        CityDto cityDto = new CityDto();
+        cityDto.setId(city.getId());
+        cityDto.setName(city.getName());
+
+        return cityDto;
+    }
+
+    private List<RegionDto> buildRegionDtos(List<Region> regions) {
+        List<RegionDto> districtDtos = new ArrayList<RegionDto>();
+        if (regions != null) {
+            for (Region region : regions) {
+                districtDtos.add(buildRegionDto(region));
+            }
+        }
+
+        return districtDtos;
+    }
+
+    private RegionDto buildRegionDto(Region region) {
+        RegionDto regionDto = new RegionDto();
+        regionDto.setId(region.getId());
+        regionDto.setCityId(region.getCityId());
+        regionDto.setName(region.getName());
+        regionDto.setParentId(region.getParentId());
+
+        return regionDto;
     }
 }
