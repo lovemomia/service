23f7f89dc5dcff0b806eb6337f86e5c4083fa97c
吴秋锋 <arthas.wuqf@gmail.com>
2015-09-24 14:23:13 +0800

    client type

--- product/service-product/src/main/java/cn/momia/service/product/web/ctrl/PaymentController.java
+++ product/service-product/src/main/java/cn/momia/service/product/web/ctrl/PaymentController.java
@@ -9,7 +9,7 @@ import cn.momia.common.deal.gateway.PayType;
 import cn.momia.common.webapp.config.Configuration;
 import cn.momia.common.webapp.ctrl.BaseController;
 import cn.momia.common.deal.gateway.CallbackParam;
-import cn.momia.common.deal.gateway.ClientType;
+import cn.momia.common.client.ClientType;
 import cn.momia.common.deal.gateway.PaymentGateway;
 import cn.momia.common.deal.gateway.PrepayParam;
 import cn.momia.common.deal.gateway.PrepayResult;
