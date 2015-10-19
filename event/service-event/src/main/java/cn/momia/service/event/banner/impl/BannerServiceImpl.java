fe6bc1c7a5056a56c9390f741101f25c7565a4cb
吴秋锋 <arthas.wuqf@gmail.com>
2015-10-19 18:31:18 +0800

    重构 event

--- event/service-event/src/main/java/cn/momia/service/event/banner/impl/BannerServiceImpl.java
+++ event/service-event/src/main/java/cn/momia/service/event/banner/impl/BannerServiceImpl.java
@@ -3,29 +3,13 @@ package cn.momia.service.event.banner.impl;
 import cn.momia.common.service.DbAccessService;
 import cn.momia.service.event.banner.Banner;
 import cn.momia.service.event.banner.BannerService;
-import org.springframework.jdbc.core.RowCallbackHandler;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class BannerServiceImpl extends DbAccessService implements BannerService {
     @Override
     public List<Banner> list(int cityId, int count) {
-        final List<Banner> banners = new ArrayList<Banner>();
-        String sql = "SELECT Cover, Action FROM SG_Banner WHERE Status=1 AND (CityId=? OR CityId=0) ORDER BY Weight DESC, AddTime DESC LIMIT ?";
-        jdbcTemplate.query(sql, new Object[] { cityId, count }, new RowCallbackHandler() {
-            @Override
-            public void processRow(ResultSet rs) throws SQLException {
-                Banner banner = new Banner();
-                banner.setCover(rs.getString("Cover"));
-                banner.setAction(rs.getString("Action"));
-
-                banners.add(banner);
-            }
-        });
-
-        return banners;
+        String sql = "SELECT * FROM SG_Banner WHERE Status=1 AND (CityId=? OR CityId=0) ORDER BY Weight DESC, AddTime DESC LIMIT ?";
+        return queryList(sql, Banner.class);
     }
 }
--- event/service-event/src/main/java/cn/momia/service/event/base/impl/EventServiceImpl.java
+++ event/service-event/src/main/java/cn/momia/service/event/base/impl/EventServiceImpl.java
@@ -3,31 +3,13 @@ package cn.momia.service.event.base.impl;
 import cn.momia.common.service.DbAccessService;
 import cn.momia.service.event.base.Event;
 import cn.momia.service.event.base.EventService;
-import org.springframework.jdbc.core.RowCallbackHandler;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class EventServiceImpl extends DbAccessService implements EventService {
     @Override
     public List<Event> list(int cityId, int count) {
-        final List<Event> events = new ArrayList<Event>();
-        String sql = "SELECT Title, Img, `Desc`, Action FROM SG_Event WHERE Status=1 AND (CityId=? OR CityId=0) ORDER BY Weight DESC, AddTime DESC LIMIT ?";
-        jdbcTemplate.query(sql, new Object[] { cityId, count }, new RowCallbackHandler() {
-            @Override
-            public void processRow(ResultSet rs) throws SQLException {
-                Event event = new Event();
-                event.setTitle(rs.getString("Title"));
-                event.setImg(rs.getString("Img"));
-                event.setDesc(rs.getString("Desc"));
-                event.setAction(rs.getString("Action"));
-
-                events.add(event);
-            }
-        });
-
-        return events;
+        String sql = "SELECT * FROM SG_Event WHERE Status=1 AND (CityId=? OR CityId=0) ORDER BY Weight DESC, AddTime DESC LIMIT ?";
+        return queryList(sql, Event.class);
     }
 }
