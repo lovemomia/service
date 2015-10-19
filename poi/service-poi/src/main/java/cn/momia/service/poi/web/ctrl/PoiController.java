4fa70d9713cd2eeb9803db31948da4eb5032ae43
吴秋锋 <arthas.wuqf@gmail.com>
2015-10-19 18:24:00 +0800

    placeDto

--- poi/service-poi/src/main/java/cn/momia/service/poi/web/ctrl/PoiController.java
+++ poi/service-poi/src/main/java/cn/momia/service/poi/web/ctrl/PoiController.java
@@ -1,7 +1,10 @@
 package cn.momia.service.poi.web.ctrl;
 
+import cn.momia.api.poi.dto.PlaceDto;
 import cn.momia.common.api.http.MomiaHttpResponse;
 import cn.momia.common.webapp.ctrl.BaseController;
+import cn.momia.service.poi.Place;
+import cn.momia.service.poi.PlaceImage;
 import cn.momia.service.poi.PlaceService;
 import com.google.common.base.Splitter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -21,7 +26,28 @@ public class PoiController extends BaseController {
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public MomiaHttpResponse get(@PathVariable int id) {
-        return MomiaHttpResponse.SUCCESS(placeService.get(id));
+        return MomiaHttpResponse.SUCCESS(buildPlaceDto(placeService.get(id)));
+    }
+
+    private PlaceDto buildPlaceDto(Place place) {
+        PlaceDto placeDto = new PlaceDto();
+        placeDto.setId(place.getId());
+        placeDto.setCityId(place.getCityId());
+        placeDto.setRegionId(place.getRegionId());
+        placeDto.setName(place.getName());
+        placeDto.setAddress(place.getAddress());
+        placeDto.setDesc(place.getDesc());
+        placeDto.setCover(place.getCover());
+        placeDto.setLng(place.getLng());
+        placeDto.setLat(place.getLat());
+
+        List<String> imgs = new ArrayList<String>();
+        for (PlaceImage img : place.getImgs()) {
+            imgs.add(img.getUrl());
+        }
+        placeDto.setImgs(imgs);
+
+        return placeDto;
     }
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
@@ -31,6 +57,15 @@ public class PoiController extends BaseController {
             ids.add(Integer.valueOf(id));
         }
 
-        return MomiaHttpResponse.SUCCESS(placeService.list(ids));
+        return MomiaHttpResponse.SUCCESS(buildPlaceDtos(placeService.list(ids)));
+    }
+
+    private List<PlaceDto> buildPlaceDtos(List<Place> places) {
+        List<PlaceDto> placeDtos = new ArrayList<PlaceDto>();
+        for (Place place : places) {
+            placeDtos.add(buildPlaceDto(place));
+        }
+
+        return placeDtos;
     }
 }
