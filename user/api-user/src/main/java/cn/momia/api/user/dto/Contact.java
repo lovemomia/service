dde1f0ddf22e5269d4b164d907e3e1dcd9594d33
吴秋锋 <arthas.wuqf@gmail.com>
2015-12-03 18:54:42 +0800

    fix bug

--- user/api-user/src/main/java/cn/momia/api/user/dto/Contact.java
+++ user/api-user/src/main/java/cn/momia/api/user/dto/Contact.java
@@ -2,19 +2,30 @@ package cn.momia.api.user.dto;
 
 import org.apache.commons.lang3.StringUtils;
 
-public class Contact extends User {
-    private User user;
-
-    public Contact(User user) {
-        this.user = user;
-    }
+public class Contact {
+    private String name;
+    private String mobile;
 
     public String getName() {
-        if (StringUtils.isBlank(user.getName())) return user.getNickName();
-        return user.getName();
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getMobile() {
-        return user.getMobile();
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public Contact() {}
+
+    public Contact(User user) {
+        this.name = StringUtils.isBlank(user.getName()) ? user.getNickName() : user.getName();
+        this.mobile = user.getMobile();
     }
 }
