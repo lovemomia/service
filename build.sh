a93c45684ca75b2172d2548d795a3793b4b9c24c
吴秋锋 <arthas.wuqf@gmail.com>
2015-12-25 14:16:20 +0800

    脚本更新

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mvn -U -pl base/api-base,course/api-course,event/api-event,feed/api-feed,im/api-im,poi/api-poi,user/api-user clean install
-mvn -U -pl base/service-base,course/service-course,event/service-event,feed/service-feed,im/service-im,poi/service-poi,user/service-user clean install
+mvn -U -pl base/api-base,course/api-course,event/api-event,feed/api-feed,im/api-im,poi/api-poi,teacher/api-teacher,user/api-user clean install
+mvn -U -pl base/service-base,course/service-course,event/service-event,feed/service-feed,im/service-im,poi/service-poi,teacher/service-teacher,user/service-user clean install
 mvn -U -pl service clean package
