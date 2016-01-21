5ca79ea9862097cc147216a8b96e6b399e0f0360
吴秋锋 <arthas.wuqf@gmail.com>
2016-01-21 15:07:12 +0800

    课程包列表页

--- course/service-course/src/main/java/cn/momia/service/course/subject/impl/SubjectServiceImpl.java
+++ course/service-course/src/main/java/cn/momia/service/course/subject/impl/SubjectServiceImpl.java
@@ -227,8 +227,8 @@ public class SubjectServiceImpl extends AbstractService implements SubjectServic
 
     @Override
     public List<Subject> list(int cityId) {
-        String sql = "SELECT Id FROM SG_Subject WHERE CityId=? AND Status=1";
-        List<Long> subjectIds = queryLongList(sql, new Object[] { cityId });
+        String sql = "SELECT Id FROM SG_Subject WHERE CityId=? AND `Type`=? AND Status=1";
+        List<Long> subjectIds = queryLongList(sql, new Object[] { cityId, Subject.Type.NORMAL });
 
         return list(subjectIds);
     }
