f4006df0dfaf475b08f02fa3d2b63b5f8638a89e
吴秋锋 <arthas.wuqf@gmail.com>
2016-02-19 14:08:29 +0800

    rm operator module

--- operate/service-operate/src/main/java/cn/momia/service/operate/config/ConfigService.java
+++
@@ -1,9 +0,0 @@
-package cn.momia.service.operate.config;
-
-import java.util.List;
-
-public interface ConfigService {
-    List<Config.Banner> listBanners(int cityId);
-    List<Config.Icon> listIcons(int cityId);
-    List<Config.Event> listEvents(int cityId);
-}
